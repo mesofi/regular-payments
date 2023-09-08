52a92a4ea987cd92dbf0dd88ff58bc847a6576d7
Armando Rivas <rivasarmando271084@gmail.com>
2023-09-08 08:04:34 -0600

    Added initial project

---
+++ src/test/java/com/mesosfi/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesosfi.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
