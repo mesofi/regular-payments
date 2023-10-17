dc67b202ad5ef5be222d84339eb15277fcc15eac
Armando Rivas <rivasarmando271084@gmail.com>
2023-10-17 06:13:37 -0600

    Added new project

--- src/main/java/com/mesofi/payments/PaymentsApplication.java
+++ src/main/java/com/mesofi/payments/PaymentsApplication.java
@@ -1,4 +1,4 @@
-package com.mesosfi.payments;
+package com.mesofi.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsApplication.class, args);
+    }
 
 }
---
+++ src/test/java/com/mesofi/payments/PaymentsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.mesofi.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("itest")
+class PaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        log.debug("Loaded correctly!!");
+    }
+
+}
